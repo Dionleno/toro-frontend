b8d31c4506ea1cf6ff6d5e4d0c4ccba1b4b63745
Dionleno <jhonsp_leno@hotmail.com>
2023-10-25 10:56:02 -0300

    ci: adding github enviroment

--- set-env.ts
+++ set-env.ts
@@ -6,13 +6,13 @@ const targetPath = './src/environments/environment.ts';
 const envConfigFile = `export const environment = {
     production: true,
     auth: {
-        domain: '${process.env.OAUTH0_DOMAIN}',
-        clientId: '${process.env.OAUTH0_CLIENT_ID}',
+        domain: '${process.env['OAUTH0_DOMAIN']}',
+        clientId: '${process.env['OAUTH0_CLIENT_ID']}',
         redirectUri: window.location.origin,
-        audience: '${process.env.OAUTH0_AUDIENCE}',
+        audience: '${process.env['OAUTH0_AUDIENCE']}',
       },
       dev: {
-        serverUrl: '${process.env.SERVER_URL}',
+        serverUrl: '${process.env['SERVER_URL']}',
       },
 }`;
 
